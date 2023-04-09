e12ee108dd84cbc0b5388128453ae78180849259
Steve Tom <tomsteve187@gmail.com>
2023-04-09 10:54:34 +0300

    Changed the files from pet names to a simple gpt

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -19,21 +19,29 @@ export default async function (req, res) {
   if (animal.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
+        message: "Please enter valid data",
       }
     });
     return;
   }
 
   try {
+
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: generatePrompt(animal),
-      temperature: 0.6,
+      temperature: 0.9,
+      max_tokens: 150,
+      top_p: 1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.6,
+      stop: [" Human:", " AI:"],
     });
+  
+    
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
-    // Consider adjusting the error handling logic for your use case
+
     if (error.response) {
       console.error(error.response.status, error.response.data);
       res.status(error.response.status).json(error.response.data);
@@ -51,12 +59,14 @@ export default async function (req, res) {
 function generatePrompt(animal) {
   const capitalizedAnimal =
     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+  return ` ${capitalizedAnimal}`;
+}
+ //function to create a random name with 6 char
+function randomName() {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let result = "";
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
 }
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,9 @@ export default function Home() {
   const [animalInput, setAnimalInput] = useState("");
   const [result, setResult] = useState();
 
+  const [messages, setMessages] = useState([]);
+
+
   async function onSubmit(event) {
     event.preventDefault();
     try {
@@ -16,13 +19,20 @@ export default function Home() {
         },
         body: JSON.stringify({ animal: animalInput }),
       });
-
+  
       const data = await response.json();
       if (response.status !== 200) {
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
-
-      setResult(data.result);
+  
+      setMessages([...messages.slice(0, -1), { message: animalInput, reply: data.result, typing: true }]);
+      setTimeout(() => {
+        setMessages((messages) =>
+          messages.map((message, index) =>
+            index === messages.length - 1 ? { ...message, typing: false } : message
+          )
+        );
+      }, data.result.length * 30); // Adjust the delay per character as needed
       setAnimalInput("");
     } catch(error) {
       // Consider implementing your own error handling logic here
@@ -30,28 +40,45 @@ export default function Home() {
       alert(error.message);
     }
   }
+  
 
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <title>Raccoon Gpt</title>
+        <link rel="icon" href="/fav.png" />
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <img src="/fav.png" className={styles.icon} />
+        <h3>kenTom™️</h3>
+        <div className={styles.chatContainer}>
+        {messages.map((item, index) => (
+            <div key={index} className={styles.messageCont}>
+              <p className={styles.sentMessage}>{item.message}</p>
+              <p className={`${styles.repliedMessage} ${item.typing ? styles.typing : ""}`}>
+                {item.reply.split("").map((char, charIndex) => (
+                  <span key={charIndex} style={{ animationDelay: `${charIndex * 0.03}s` }}>
+                    {char}
+                  </span>
+                ))}
+              </p>
+            </div>
+          ))}
+
+          </div>
         <form onSubmit={onSubmit}>
           <input
             type="text"
             name="animal"
-            placeholder="Enter an animal"
+            placeholder="Enter a prompt"
             value={animalInput}
             onChange={(e) => setAnimalInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Reply" />
         </form>
         <div className={styles.result}>{result}</div>
+        
       </main>
     </div>
   );
